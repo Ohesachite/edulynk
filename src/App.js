47e3a071fbb78c65038bca013d3d887aea52430c
Ohesachite <Alan2001liu09@yahoo.com>
2020-08-08 14:33:43 -0400

    Background experiment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello world!</h1>
+        <p>Sujit needs to work on his personal account.</p>
       </header>
+      <h1>Not in header or footer</h1>
+      <p>What's up</p>
+      <p>Hello world!</p>
+      <p>Goodbye world!</p>
+      <footer className="App-footer">
+        <p>Author: John Doe</p>
+      </footer>
     </div>
   );
 }
