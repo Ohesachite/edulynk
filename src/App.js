22a9333b735819e2a09e0ac7a2ccd90c57c33366
Ohesachite <Alan2001liu09@yahoo.com>
2020-08-08 16:58:22 -0400

    Necessary commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
+// import { Switch, Route } from 'react-router-dom';
 import './App.css';
-//import Sidebar from "./components/sidebar";
+// import './components/Bootstrap/css/s'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Sidebar from "./components/sidebar.js";
+import Header from "./components/header.js";
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello world!</h1>
-          <p>Sujit needs to work on his personal account.</p>
-        </header>
-        <h1>Not in header or footer</h1>
-        <p>What's up</p>
-        <p>Hello world!</p>
-        <p>Goodbye world!</p>
+      <div>
+        {/* <Switch>
+         <Route exact path="/" component={Home} />
+        </Switch> */}
+        <Header />
+        <Sidebar />
+        <h1>Hello World!</h1>
         <footer className="App-footer">
           <p>Author: John Doe</p>
         </footer>
       </div>
+
     );
   }
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './css/sidebar.css';
+
 
 class Sidebar extends Component {
-    render(){
+    render() {
         return (
             //HTML Stuff HERE
-            
+
             <div class="d-flex" id="wrapper">
-            <div class="bg-light border-right" id="sidebar-wrapper">
-                <div class="sidebar-heading">Side Menu </div>
+                <div class="bg-light border-right" id="sidebar-wrapper">
+                    <div class="sidebar-heading">Side Menu </div>
                     <div class="list-group list-group-flush">
                         <a href="#" class="list-group-item list-group-item-action bg-light">Classes Home</a>
                         <a href="#" class="list-group-item list-group-item-action bg-light">Class 1</a>
@@ -16,11 +19,11 @@ class Sidebar extends Component {
                         <a href="#" class="list-group-item list-group-item-action bg-light">Settings</a>
                         <a href="#" class="list-group-item list-group-item-action bg-light">Calendar</a>
                     </div>
-                </div> 
-             </div> 
-            
+                </div>
+            </div>
+
             //end of HTML stuff
         );
     }
-        
+
 } export default Sidebar;
