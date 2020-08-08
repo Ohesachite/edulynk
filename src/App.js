1d59cc76e3d70b883981a743d9f430544b63a9c2
Karan Menon <krmenon@usc.edu>
2020-08-08 16:50:48 -0400

    moved out bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+// import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import Sidebar from "./components/sidebar";
+// import './components/Bootstrap/css/s'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Sidebar from "./components/sidebar.js";
+import Header from "./components/header.js";
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        {/* <Switch>
+         <Route exact path="/" component={Home} />
+        </Switch> */}
+        <Header />
+        <Sidebar />
+        <h1>Hello World!</h1>
+        <footer className="App-footer">
+          <p>Author: John Doe</p>
+        </footer>
       </div>
+
     );
   }
 }
