bd2aac32ada2e40f66bc82dd1e008c63a7874231
Karan Menon <krmenon@usc.edu>
2020-08-08 16:53:49 -0400

    resolving merge conflict

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import Sidebar from "./components/sidebar.js";
 import Header from "./components/header.js";
 
 
-
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <div>
         {/* <Switch>
          <Route exact path="/" component={Home} />
@@ -19,17 +17,6 @@ class App extends Component {
         <Header />
         <Sidebar />
         <h1>Hello World!</h1>
-=======
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello world!</h1>
-          <p>Sujit needs to work on his personal account.</p>
-        </header>
-        <h1>Not in header or footer</h1>
-        <p>What's up</p>
-        <p>Hello world!</p>
-        <p>Goodbye world!</p>
->>>>>>> fb5e562bb881c0ab5851536ea64c16056fcb4331
         <footer className="App-footer">
           <p>Author: John Doe</p>
         </footer>
