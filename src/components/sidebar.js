fa43d613a9841b1c86acf458febe16d39c84ecd7
Ohesachite <Alan2001liu09@yahoo.com>
2020-08-09 20:12:16 -0400

    Merge conflict resolve

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import "../assets/sidebar.css";
 
@@ -46,37 +45,3 @@ class Sidebar extends Component {
    }
 }
 export default Sidebar;
-=======
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './css/sidebar.css';
-
-
-class Sidebar extends Component {
-    render() {
-        return (
-            //HTML Stuff HERE
-
-                < div className = "d-flex" id = "wrapper" >
-                    <div className="bg-light border-right" id="sidebar-wrapper">
-                        <div className="sidebar-heading">Side Menu </div>
-                        <div className="list-group list-group-flush">
-                            <a href="#" className="list-group-item list-group-item-action bg-light">Classes Home</a>
-                            <a href="#" className="list-group-item list-group-item-action bg-light">Class 1</a>
-                            <a href="#" className="list-group-item list-group-item-action bg-light">Class 2</a>
-                            <a href="#" className="list-group-item list-group-item-action bg-light">Class 3</a>
-                            <a href="#" className="list-group-item list-group-item-action bg-light">Settings</a>
-                            <a href="#" className="list-group-item list-group-item-action bg-light">Calendar</a>
-                        </div>
-                    </div>
-            </div >
-
-            //end of HTML stuff
-
-
-
-        );
-    }
-
-} export default Sidebar;
->>>>>>> 28a4e83b8c69602fbe475a8919c8881362e15de2
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -11,19 +10,6 @@ ReactDOM.render(
       <App />
    </React.StrictMode>,
    document.getElementById("root")
-=======
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
->>>>>>> 28a4e83b8c69602fbe475a8919c8881362e15de2
 );
 
 // If you want your app to work offline and load faster, you can change
