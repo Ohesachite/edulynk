f8ad2dc1fd6d91d72d327beb0392d7f2da595bf7
shreesamavedhi <shreesamavedhi@114@gmail.com>
2020-08-09 21:09:05 -0500

    links and design updates

--- src/problemInput.js
+++ src/problemInput.js
@@ -1,6 +1,21 @@
 // this is code for our main feature: finding ways to upload
 //math problems
 
+/*
+This is a dynamic form, meaning that the user will
+be able to decide how many inputs they want
+*/
+
+import React, {Component} from "react";
+
 class ProblemInput extends Component{
-    
+    render(){
+        return(
+            <form>
+                <label> </label>
+                <label> </label>
+
+            </form>
+        )
+    }
 }
